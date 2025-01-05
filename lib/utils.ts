83a22d5b534f630db203bb25e2cb5a47df90b2d2
itsbrunodev <brunka215@gmail.com>
2025-01-05 20:05:39 +0100

    Add kebab case formatter utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,6 +19,14 @@ export function toTitleCase(str: string) {
     );
 }
 
+export function toKebabCase(str: string) {
+  return str
+    .replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, "")
+    .replace(/([a-z])([A-Z])/g, (m, a, b) => `${a}_${b.toLowerCase()}`)
+    .replace(/[^A-Za-z0-9]+|_+/g, "-")
+    .toLowerCase();
+}
+
 export function shadeColor(color: string, decimal: number): string {
   const base = color.startsWith("#") ? 1 : 0;
 
