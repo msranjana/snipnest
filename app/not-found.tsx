c18b41631fc7e6d4181a39bcef02a9811e41c120
itsbrunodev <brunka215@gmail.com>
2025-01-05 18:36:45 +0100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,19 @@
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 
-import { NotFound } from "@/components/not-found";
+// import { NotFound } from "@/components/not-found";
+
+const NotFoundWithNoSSR = dynamic(
+  () => import("../components/not-found").then((x) => x.NotFound),
+  {
+    ssr: false,
+  },
+);
 
 export const metadata: Metadata = {
   title: "Not Found - SnipNest",
 };
 
-export default async function NotFoundPage() {
-  return <NotFound />;
+export default function NotFoundPage() {
+  return <NotFoundWithNoSSR />;
 }
