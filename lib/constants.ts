2f4fa3043893589f6d84946177bcf18a79a79e01
itsbrunodev <brunka215@gmail.com>
2025-01-09 17:17:02 +0100

    Add python to languages list

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,7 @@
 import Javascript from "devicons-react/lib/icons/JavascriptOriginal";
 import Typescript from "devicons-react/lib/icons/TypescriptOriginal";
 import Rust from "devicons-react/lib/icons/RustOriginal";
+import Python from "devicons-react/lib/icons/PythonOriginal";
 import React from "devicons-react/lib/icons/ReactOriginal";
 
 /**
@@ -24,6 +25,12 @@ export const LANGUAGES = [
     value: "rust",
     icon: Rust,
     isColored: false,
+  },
+  {
+    name: "Python",
+    value: "python",
+    icon: Python,
+    isColored: true,
   },
   /**
    * Languages below aren't languages, but for the
