b88fa49a82e70b77b7e301057aea800cb57ac9ce
itsbrunodev <brunka215@gmail.com>
2025-01-12 12:11:09 +0100

    Format layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,11 @@ export default async function RootLayout({
   const snippetList = getSnippetList(groupedSnippets);
 
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html
+      lang="en"
+      className="scroll-smooth"
+      suppressHydrationWarning
+    >
       <body
         className={cn(
           "antialiased bg-zinc-300 dark:bg-black",
