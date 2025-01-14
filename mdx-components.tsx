16d81dadc425bb5d0f7eb6e4af665ceb301ba006
itsbrunodev <brunka215@gmail.com>
2025-01-14 21:18:33 +0100

    Remove the use of return in mdx-components.tsx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -15,22 +15,20 @@ export function useMDXComponents(components: MDXComponents) {
     ...components,
     pre: (
       props: JSX.IntrinsicElements["pre"] & { "data-language"?: string }
-    ) => {
-      return (
-        <>
-          <figcaption className="flex items-center justify-between bg-card border text-sm border-border rounded-t-md px-4 py-2">
-            <span className="text-muted-foreground">
-              {props["data-language"]}
-            </span>
-            <CopyButton content={reactToString(props.children)} />
-          </figcaption>
-          <pre
-            className="relative bg-card border border-border border-t-0 rounded-b-md px-0 py-4 focus-visible:outline-none"
-            {...props}
-          />
-        </>
-      );
-    },
+    ) => (
+      <>
+        <figcaption className="flex items-center justify-between bg-card border text-sm border-border rounded-t-md px-4 py-2">
+          <span className="text-muted-foreground">
+            {props["data-language"]}
+          </span>
+          <CopyButton content={reactToString(props.children)} />
+        </figcaption>
+        <pre
+          className="relative bg-card border border-border border-t-0 rounded-b-md px-0 py-4 focus-visible:outline-none"
+          {...props}
+        />
+      </>
+    ),
     Code: (props: CodeTabProps) => <Code {...props} />,
     Snippet: (props: CodeTabProps) => <CodeSnippet {...props} />,
     Example: (props: CodeTabProps) => <CodeExample {...props} />,
