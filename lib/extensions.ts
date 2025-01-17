16ffe90d5ede142d1817005a3239b735137e2776
itsbrunodev <brunka215@gmail.com>
2025-01-17 19:02:03 +0100

    Move extensions constant out from extensions route into its own file

---
+++ lib/extensions.ts
@@ -0,0 +1,7 @@
+export const EXTENSIONS = {
+  vscode: {
+    name: "SnipNest for Visual Studio Code",
+    link: "https://marketplace.visualstudio.com/items?itemName=itsbrunodev.snipnest",
+    icon: "https://raw.githubusercontent.com/itsbrunodev/snipnest/refs/heads/main/assets/vscode.png",
+  },
+};
